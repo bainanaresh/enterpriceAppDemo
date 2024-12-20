e846d42f43222de7b34d8fda159dceb8cc1eec7c
bainanaresh <bainanaresh407@gmail.com>
2024-12-20 10:18:51 +0530

    initial commit

---
+++ src/main/java/com/baina/EnterpriceAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.baina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriceAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriceAppDemoApplication.class, args);
+	}
+
+}
